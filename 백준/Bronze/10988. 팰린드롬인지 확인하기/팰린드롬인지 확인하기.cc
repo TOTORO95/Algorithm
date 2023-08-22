d0e5abf7986105c1293d322968aafd303024bec9
TOTORO95 <126373491+TOTORO95@users.noreply.github.com>
2023-08-22 21:58:54 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string str, reverStr;
+    cin >> str;
+
+    reverStr = str;
+    reverse(reverStr.begin(), reverStr.end());
+
+    if (reverStr == str) {
+        cout << 1 << endl;
+    } 
+    else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}

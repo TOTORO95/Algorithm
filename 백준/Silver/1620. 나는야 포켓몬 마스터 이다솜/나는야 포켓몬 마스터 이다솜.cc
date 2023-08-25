5e4a027dd336dcf41cb9bc3b98dedfad49073444
TOTORO95 <126373491+TOTORO95@users.noreply.github.com>
2023-08-26 03:01:43 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 156 ms, Memory: 20868 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int pokemonNum, problemNum;
+    string name;
+    map<string, int> nameMap;
+    vector<string> nameVec(100004, "");
+
+    cin >> pokemonNum >> problemNum;
+    for (int i = 0; i < pokemonNum; i++) {
+        cin >> name;
+        nameMap.insert({name, i + 1});
+        nameVec[i + 1] = name;
+    }
+
+    for (int i = 0; i < problemNum; i++) {
+        cin >> name;
+        int nameNo = atoi(name.c_str());
+        if (nameNo == 0) {
+            cout << nameMap[name] << "\n";
+        } else {
+            cout << nameVec[nameNo] << "\n";
+        }
+    }
+
+    return 0;
+}

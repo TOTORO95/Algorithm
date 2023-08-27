9e6e2f30d5c3315edeeeb719283ab3cfe7ecc5a0
cat5778@naver.com <>
2023-08-27 18:28:39 +0900

    Mac Build Setting

---
+++ Study/Test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+
+    int a =3 ;
+
+    cout<< a << "\n";
+
+    cin >> a ;
+
+    for(int i=0; i<10; i++ ) {
+        ++a;
+        cout << a<<"\n";
+    }
+
+
+}
